3e4e1bc65679fe8a07245a2c60e255970a0bf389
lngka <foxlead@gmail.com>
2017-10-07 10:02:06 +0200

    default hidding of loading div

--- app/controllers/client.loadLastSearch.js
+++ app/controllers/client.loadLastSearch.js
@@ -6,21 +6,28 @@
 onDOMready(function() {
     let searchField = document.querySelector("#searchField");
 
-    try { //if user is logged in
+    try { //#lastSearchedLocation is rendered if user logged in
         let location = document.querySelector("#lastSearchedLocation").innerHTML;
         searchField.value = location;
-        //eslint-disable-next-line no-undef
-        displayNightspotsByLocation(location, function(err) {// defined in client.searchController.js
-            if (err) alert(err.message);
-        });
-    } catch (e) {
-        try { // we may have saved a cookie for this before
-            let location = document.querySelector("#cookieLocation").innerHTML;
-            searchField.value = location;
+
+        if (location) {
             //eslint-disable-next-line no-undef
-            displayNightspotsByLocation(location, function(err) {// defined in client.searchController.js
+            displayNightspotsByLocation(location, function(err) {//defined in client.searchController.js
                 if (err) alert(err.message);
             });
+        }
+    } catch (e) {
+        try { // even if not logged in...
+            let location = document.querySelector("#cookieLocation").innerHTML;
+            searchField.value = location;
+        
+            //there might be a cookie from last user's session
+            if (location) {
+                //eslint-disable-next-line no-undef
+                displayNightspotsByLocation(location, function(err) {// defined in client.searchController.js
+                    if (err) alert(err.message);
+                });
+            }
         } catch (e) {
             return console.log(e);
         }
